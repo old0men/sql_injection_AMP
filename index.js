131589bb017b04b02327d23f9ff0c08790143178
IM23a-buerkim <buerkim@bzz.ch>
2025-10-22 13:47:51 +0200

    index.js update MB 22.10 13:47

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const pool = require('./db');
 
 async function test() {
   try {
-    const [rows] = await pool.query('SELECT User_id, Name, Last_Name FROM `user` LIMIT 10;');
+    const [rows] = await pool.query('SELECT * FROM user JOIN data ON user.user_id = data.user_id;');
     console.log('Ergebnis:', rows);
   } catch (err) {
     console.error('Fehler:', err.code, err.message);
